60973294c3e7d15b4245fdc10f46d7f39e40f132
ksjdjdjdn8182 <1668964916@qq.com>
2026-07-25 13:52:11 +0800

    Update diy.sh

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -71,10 +71,10 @@ coremark wget-ssl curl autocore htop nano zram-swap kmod-lib-zstd kmod-tcp-bbr b
 
 sed -i "s/^.*vermagic$/\techo '1' > \$(LINUX_DIR)\/.vermagic/" include/kernel-defaults.mk
 
-# 直接固定分支，完全删除GraphQL API查询代码，不再请求github接口
+# 直接固定分支，完全移除所有GraphQL查询代码
 REPO_BRANCH="openwrt-25.12"
 
-# 【建议删除此段，彻底避免API限流】等待kiddin9插件编译循环
+# 注释掉kiddin9 API等待循环，不再请求github接口
 # status=$(curl -H "Authorization: token $REPO_TOKEN" -s "https://api.github.com/repos/kiddin9/op-packages/actions/runs" | jq -r '.workflow_runs[0].status')
 # echo "$status"
 # while [[ "$status" == "in_progress" || "$status" == "queued" ]];do
